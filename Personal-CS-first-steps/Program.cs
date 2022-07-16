289bb53725d8a7eb508d5beb3c56891d91013e11
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-07-16 09:49:44 +0200

    vars

--- Personal-CS-first-steps/Program.cs
+++ Personal-CS-first-steps/Program.cs
@@ -8,12 +8,39 @@ namespace Personal_CS_first_steps
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Funziona");
             Console.Write("Sono Wirte. ");
             Console.Write("Non creo nuove righe. ");
-            Console.Write("Quando mi usi occhio agli spazzi ;D. ");
+            Console.Write("Quando mi usi occhio agli spazzi ;D. "); //comment
+            //commento
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            /*
+             multi line
+             */
+
+            //variabili
+            //integers
+            int number = 1;
+            Console.WriteLine(number);
+
+            // integers with ,
+            double price = 22.11;
+            Console.WriteLine(price);
+
+            //single charachters... (solo apici singoli)
+            char letter = 'A';
+            Console.WriteLine(letter);
+
+            //texts
+            string text = "Ciao bello";
+            Console.WriteLine(text);
+
+
         }
     }
 }
